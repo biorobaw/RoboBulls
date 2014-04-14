30eed20166b048440d9e2d8aa125d851ec6b721e
Administrator <administrator@c010999-lin.(none)>
2014-04-14 19:50:56 -0400

    Measurment fixed

--- Measure/measurments.cpp
+++ Measure/measurments.cpp
@@ -6,5 +6,5 @@ Measurments::Measurments()
 
 float Measurments::distance(Point p1, Point p2)
 {
-    return sqrt(pow((p1.getX()-p2.getX()),2) + pow((p2.getY()-p2.getY()), 2));
+    return sqrt(pow((p1.getX()-p2.getX()),2) + pow((p1.getY()-p2.getY()), 2));
 }
