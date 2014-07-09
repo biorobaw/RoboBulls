0008dda54d413996738062663b0bf332c6c6c147
Narges Ghaedi <nghaedi@mail.usf.edu>
2014-07-09 14:56:30 -0400

    Drive ball and kick toward the goal is complete

--- skill/driveball.cpp
+++ skill/driveball.cpp
@@ -10,28 +10,6 @@
 
 namespace Skill
 {
-//    static int gotoBallUpdateFn(Skill::Skill& s, Robot* rob)
-//	{
-//		s = GoToPosition(GameModel::getModel()->getBallPoint(), DriveBall::direction);
-//		return -1;
-//	}
-
-//	DriveBall::DriveBall(Point targetPoint, double movingDirection)
-//	{
-//		Point ballPosition = GameModel::getModel()->getBallPoint();
-//		direction = movingDirection;
-
-//		mySkillSequence.addSkill( new GoToPosition(ballPosition,direction), gotoBallUpdateFn);
-//		mySkillSequence.addSkill( new GoToPosition(targetPoint,direction) );
-//		mySkillSequence.addSkill( new Stop() );
-//	}
-
-//	void DriveBall::perform(Robot* robot)
-//	{
-//		cout << "driving ball" <<endl;
-
-//		mySkillSequence.executeOn(robot);
-//	}
     DriveBall::DriveBall(Point targetPoint, double finalDirection)
     {
         targetPosition = targetPoint;
@@ -41,46 +19,8 @@ namespace Skill
 
     bool DriveBall::perform(Robot* robot)
     {
-//        Point goal(-3000, 0);
         GameModel *gm = GameModel::getModel();
-
-//        Point ball = gm->getBallPoint();
-//        Point goal = gm->getOpponentGoal();
-//        float targetToBallAngle = Measurments::angleBetween(targetPosition,ball);
-//        float ballTotargetAngle = Measurments::angleBetween(ball, targetPosition);
         Point *behindBall;
-
-//        Skill::Skill * skill;
-//        switch (state) {
-//        case goingBehind:
-//            skill = new Skill::GoToPositionWithOrientation(behindBall, ballToGoal);
-//            break;
-//        case approaching:
-//            skill = new Skill::GoToPositionWithOrientation(ball, ballToGoal);
-//            break;
-//        case kicking:
-//            skill = new Skill::Kick();
-//            break;
-//        }
-
-//        // Perform the skill
-//        skill->perform(r);
-
-//        // Evaluate possible transitions
-//        switch (state){
-//        case goingBehind:
-//            if (Measurments::distance(behindBall, r->getRobotPosition()) < 100 && abs(Measurments::angleDiff(r->getOrientation(), ballToGoal)) < 10 * M_PI/180)
-//                state = approaching;
-//            break;
-//        case approaching:
-//            if (Measurments::distance(ball, r->getRobotPosition()) < 110 && abs(Measurments::angleDiff(r->getOrientation(), ballToGoal)) < 10 * M_PI/180)
-//                state = kicking;
-//            else if (Measurments::distance(ball, r->getRobotPosition()) > 200)
-//                state = goingBehind;
-//            break;
-//        case kicking:
-//            state = goingBehind;
-//        }
 
         switch(state)
         {
@@ -92,8 +32,7 @@ namespace Skill
             break;
         case moveTowardBall:
             cout <<"Move toward the ball"<<endl;
-            if(Measurments::isClose(robot->getRobotPosition(), gm->getBallPoint(), 110) /*&&
-                    abs(Measurments::angleDiff(robot->getOrientation(), Measurments::angleBetween(gm->getBallPoint(), targetPosition))) < 10 * M_PI/180*/) {
+            if(Measurments::isClose(robot->getRobotPosition(), gm->getBallPoint(), 110)) {
                 state = driveBall;
                 skill = new GoToPositionWithOrientation (targetPosition, direction);
             }
