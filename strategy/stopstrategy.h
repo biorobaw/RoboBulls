ccb98ee070588369ece8176670f10d2399fefee0
JW <p00n3dj002@yahoo.com>
2014-10-10 20:29:50 -0400

    StopStrategy rewrite and fix

--- strategy/stopstrategy.h
+++ strategy/stopstrategy.h
@@ -1,16 +1,13 @@
 #ifndef STOPSTRATEGY_H
 #define STOPSTRATEGY_H
 
-#include <unordered_map>
 #include "strategy/strategy.h"
 #include "model/gamemodel.h"
 #include "behavior/behavior.h"
 
-
 class StopStrategy:public Strategy
 {
 public:
-    StopStrategy();
     void assignBeh() override;
     bool update() override;
 
@@ -19,18 +16,10 @@ private:
      * for StopBehavior need to be rebuilt.
      */
     void rebuildTargetPoints();
-
-
-    /* Contains all the points that each robot should be going to
-     * during StopBehavior. Mapped by ID.
-     */
-    static std::unordered_map<int, Point> robTargetPoints;
-
-
     /* Need to keep track of the previous ball point, and rebuild target
      * points on a significant change
      */
-    static Point prevBallPoint;
+    Point prevBallPoint;
 };
 
 #endif // STOPSTRATEGY_H
