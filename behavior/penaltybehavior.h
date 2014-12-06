8829f7c8120d618fdcd2f43c0d3246a54de8bd81
Narges Ghaedi <nghaedi@mail.usf.edu>
2014-12-06 15:22:23 +0000

    robot kicks to opposite position of golie in penalty beh.

--- behavior/penaltybehavior.h
+++ behavior/penaltybehavior.h
@@ -14,7 +14,10 @@
 
 /*
  * Penalty Behavior
- * The which has been chosen to kick the ball during penalty (in our case first robot entering the team)
+ * The which has been chosen to kick the ball during penalty (in our the robot
+ * that is the closest to the ball at that moment excluding goalie)
+ * The robots kicks the ball toward the goal in the opposite position
+ * of opponent team's goalie
  * would move toward the ball, stops when it reaches the ball, and kicks the ball.
  * We assume the ball is at the penalty point
  * Narges Ghaedi
