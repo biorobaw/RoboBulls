a250aab2a049b5250ea2df1bc6864831240c47fe
JW <p00n3dj002@yahoo.com>
2015-02-18 13:19:26 -0500

    Omni robot changes to NxtRobComm

--- communication/nxtrobcomm.cpp
+++ communication/nxtrobcomm.cpp
@@ -34,6 +34,8 @@ void NXTRobComm::send(char* commptr, int size)
     Xbee.Write(commptr, size);
 }
 
+static int k = 4;
+
 void NXTRobComm::sendVelsLarge(std::vector<Robot*>& robots)
 {
     // Create array of packets
@@ -51,20 +53,20 @@ void NXTRobComm::sendVelsLarge(std::vector<Robot*>& robots)
         // Load information into the packet
         packet_t* packet = &teamPacketBuf[i];
         Robot* rob =  robots[i];
-        packet->tilde = '~';
+        packet->tilde = char(250);
         packet->id = rob->getID();
-        packet->left_front  = Measurments::clamp(rob->getLF(), -127, 127);
-        packet->left_back   = Measurments::clamp(rob->getLB(), -127, 127);
-        packet->right_front = Measurments::clamp(rob->getRF(), -127, 127);
-        packet->right_back  = Measurments::clamp(rob->getRB(), -127, 127);
-        packet->kick = rob->getKick() ? 'k' : 0;
-        packet->chip_power = 0;
-        packet->dribble_power = rob->getDrible() ? 1 : 0;
-        packet->dollar = '$';
+        packet->left_front  = Measurments::clamp(rob->getLF(), -127, 127)/k + 100;
+        packet->left_back   = Measurments::clamp(rob->getLB(), -127, 127)/k + 100;
+        packet->right_front = Measurments::clamp(rob->getRF(), -127, 127)/k + 100;
+        packet->right_back  = Measurments::clamp(rob->getRB(), -127, 127)/k + 100;
+        packet->kick = rob->getKick() ? 1 : 0;
+        packet->chip_power = 20;
+        packet->dribble_power = 0;
+        packet->dollar = char(255);
 
         // Ryan overriding the zero-reset in the event of a manual override from the GUI
         if (!GuiInterface::getGuiInterface()->isOverriddenBot()[robots[i]->id]) {
-            // Clear robot information
+            // Clear robot i/4nformation
             rob->setL(0);
             rob->setR(0);
             rob->setKick(0);
