09a61ddc84ec2f0bba5231aecb600e262429c963
JW <p00n3dj002@yahoo.com>
2015-04-10 14:19:31 -0400

    Changed Pathfinding to filter all paths and cleaned

--- movement/pathfinding/fppa_pathfinding.cpp
+++ movement/pathfinding/fppa_pathfinding.cpp
@@ -23,17 +23,17 @@ namespace FPPA
 {
 namespace impl
 {
-    const Point fieldTopLeft = Point(-3000, -2000);
-    const Point fieldBotRight = Point(3000, 2000);
+    const Point fieldTopLeft = Point(-FIELD_LENGTH, -FIELD_WIDTH);
+    const Point fieldBotRight = Point(FIELD_LENGTH, FIELD_WIDTH);
     int framesUntilUpdate = 0;
     std::vector<Point> currentFrameObstacles;
 
     static bool insideRadiusRectangle(const Point& p0, const Point& p1, const Point& p2)
     {
-        float rectTop = std::min(p1.y, p2.y) - ROBOT_RADIUS;
+        float rectTop    = std::min(p1.y, p2.y) - ROBOT_RADIUS;
         float rectBottom = std::max(p1.y, p2.y) + ROBOT_RADIUS;
-        float rectLeft = std::min(p1.x, p2.x) - ROBOT_RADIUS;
-        float rectRight = std::max(p1.x, p2.x) + ROBOT_RADIUS;
+        float rectLeft   = std::min(p1.x, p2.x) - ROBOT_RADIUS;
+        float rectRight  = std::max(p1.x, p2.x) + ROBOT_RADIUS;
 
         return (p0.x > rectLeft && p0.x < rectRight)
             && (p0.y > rectTop  && p0.y < rectBottom);
@@ -46,26 +46,17 @@ namespace impl
     {
         bool  obstacle_found = false;
         Point obstacle_position;
-        /* As much as I'd hate doing this, it's needed for now. */
-        GameModel* gm = GameModel::getModel();
 
         for(const Point& pt : currentFrameObstacles)
         {
-            /* With the new architecture, we need to manually exclude points that are
-             * close to the start or ending point in this function.
-             * Also... now it _always_ avoids the ball, even if it is close to the start/end
-             * when avoidBall is on
+            /* Here we exclude obstacle points that are too close to the starting point,
+             * or exclude the ball if avoidBall is not on.
              */
-            if(Measurments::isClose(pt, beginPos, ROBOT_RADIUS)
-               //Measurments::isClose(pt, endPos, ROBOT_RADIUS)
-                    )
-            {
-                if(Measurments::isClose(gm->getBallPoint(), pt, 80)) {
-                    if(not(avoidBall))
-                        continue;
-                } else {
-                    continue;
-                }
+            if(Measurments::isClose(pt, beginPos, ROBOT_RADIUS)) {
+                continue;
+            }
+            if(Measurments::isClose(gameModel->getBallPoint(), pt, 80) and not(avoidBall)) {
+                continue;
             }
 
             obstacle_found = Measurments::lineDistance(pt, beginPos, endPos) < ROBOT_SIZE*0.75 &&
@@ -78,7 +69,7 @@ namespace impl
         }
 
     #if FPPA_DEBUG
-        //printf("[FPPA] Checking Line (%f,%f)-(%f,%f)\n", beginPos.x, beginPos.y, endPos.x, endPos.y);
+        printf("[FPPA] Checking Line (%f,%f)-(%f,%f)\n", beginPos.x, beginPos.y, endPos.x, endPos.y);
         if(obstacle_found)
             printf("[FPPA] Found: %.0f,%.0f\n", obstacle_position.x, obstacle_position.y);
     #endif
@@ -152,9 +143,8 @@ namespace impl
      */
     static void buildObstacleCollection()
     {
-        GameModel* mod = GameModel::getModel();
-        const auto& myTeam = mod->getMyTeam();
-        const auto& opTeam = mod->getOponentTeam();
+        const auto& myTeam = gameModel->getMyTeam();
+        const auto& opTeam = gameModel->getOponentTeam();
 
         currentFrameObstacles.clear();
         currentFrameObstacles.reserve(myTeam.size() + opTeam.size() + 1);
@@ -165,8 +155,8 @@ namespace impl
             currentFrameObstacles.push_back(rob->getRobotPosition());
 
     #if FPPA_DEBUG
-           // std::cout << "[FPPA] All Obstacles: " << std::endl;
-            //for(Point pt : *obstacles) std::cout << pt.toString() << std::endl;
+           std::cout << "[FPPA] All Obstacles: " << std::endl;
+           for(Point pt : *obstacles) std::cout << pt.toString() << std::endl;
     #endif
     }
 
@@ -190,15 +180,10 @@ namespace impl
 
     /*********************************************************/
 
-    static bool isValidPath(const Path& p)
-    {
-        return std::none_of(p.begin(), p.end(), Comparisons::isPointOutsideField());
-    }
-
     static void sanitizePoint(Point& pt)
     {
-        pt.x = Measurments::clamp(pt.x, -2900.0f, 2900.0f);
-        pt.y = Measurments::clamp(pt.y, -1900.0f, 1900.0f);
+        pt.x = Measurments::clamp(pt.x, -FIELD_LENGTH+100.f, FIELD_LENGTH-100.f);
+        pt.y = Measurments::clamp(pt.y, -FIELD_WIDTH+100.f,  FIELD_WIDTH-100.f);
     }
 
     static float getPathLength(const Path& p)
@@ -233,29 +218,21 @@ namespace impl
         if(avoidBall)
             impl::currentFrameObstacles.push_back(GameModel::getModel()->getBallPoint());
 
-        std::pair<Path, Path> foundPaths = impl::findBothPaths(start, end, avoidBall);
+        std::pair<Path,Path> foundPaths = impl::findBothPaths(start, end, avoidBall);
 
         if(avoidBall)
             impl::currentFrameObstacles.pop_back();
+
+        //Limit all points in both paths paths to being inside the field
+        std::for_each(foundPaths.first.begin(), foundPaths.first.end(), impl::sanitizePoint);
+        std::for_each(foundPaths.second.begin(), foundPaths.second.end(), impl::sanitizePoint);
 
         //Create PathInfos: pair  of {points vector, direction top/bottom}
         //Get distance and valid status of each
         PathInfo topPathInfo = std::make_pair(foundPaths.first, PathDirection::Top);
         PathInfo botPathInfo = std::make_pair(foundPaths.second, PathDirection::Bottom);
-        bool topValidStatus  = impl::isValidPath(topPathInfo.first);
-        bool botValidStatus  = impl::isValidPath(botPathInfo.first);
         float topTotalDist   = impl::getPathLength(topPathInfo.first);
         float botTotalDist   = impl::getPathLength(botPathInfo.first);
-
-        //Top path is valid and bottom isn't; defaut top
-        if(topValidStatus and not(botValidStatus)) {
-            return topPathInfo;
-        }
-
-        //Bottom path is valid and top isn't
-        if(botValidStatus and not(topValidStatus)) {
-            return botPathInfo;
-        }
 
         //User requested top or bottom path explicitly
         if(pathHint != PathDirection::None) {
@@ -280,15 +257,9 @@ namespace impl
          }
 
         //Otherwise, selects PathInfo top or bottom strictly based on best distance.
-        //Sanitizes Points in case both are invalid.
         PathInfo& chosenPathInfo = topPathInfo;
         if(botTotalDist < topTotalDist)
             chosenPathInfo = botPathInfo;
-        Path& chosenPath = chosenPathInfo.first;
-
-        //Limit all points in the "chosen" path to being inside the field
-        std::for_each(chosenPath.begin(), chosenPath.end(), impl::sanitizePoint);
-
         return chosenPathInfo;
     }
 
@@ -296,7 +267,7 @@ namespace impl
     bool isObstacleInLine(const Point& start, const Point& end, Point* obsPosOut, bool avoidBall)
     {
         if(avoidBall)
-            impl::currentFrameObstacles.push_back(GameModel::getModel()->getBallPoint());
+            impl::currentFrameObstacles.push_back(gameModel->getBallPoint());
 
         auto obstacle_info = impl::isObstacleinLine(start, end, avoidBall);
 
