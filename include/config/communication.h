16b73649ca3b532de3c7a6f1b7e432c24e79b4ce
isMunim <abdulmunimz@mail.usf.edu>
2018-05-03 00:07:22 -0400

    Config for team blue

--- include/config/communication.h
+++ include/config/communication.h
@@ -13,9 +13,10 @@
 
 //! @brief Vision listen address options
 //! @{
-#define VISION_ADDRESS_SHAMSI "224.5.23.4"
+#define VISION_ADDRESS_ABDUL "224.5.23.4"
 #define VISION_ADDRESS_JAMES  "224.5.23.3"
 #define VISION_ADDRESS_ADRIANO "224.5.23.1"
+#define VISION_ADDRESS_SHAUGHN "224.5.23.2"
 
 //Should not be the same as any of the simulators
 #define VISION_ADDRESS_BIOROB "224.5.23.2"
@@ -36,8 +37,12 @@
 #define SIMULATOR_PORT      20011
 
 #if SIMULATED
- #define VISION_ADDRESS     VISION_ADDRESS_SHAMSI
- #define VISION_PORT        10020
+#define VISION_ADDRESS     VISION_ADDRESS_ABDUL
+#define VISION_PORT        10020
+
+//#if SIMULATED
+// #define VISION_ADDRESS     VISION_ADDRESS_SHAUGHN
+// #define VISION_PORT        10020
 #else
  #define VISION_ADDRESS     VISION_ADDRESS_BIOROB
  #define VISION_PORT        10002
@@ -50,7 +55,7 @@
 
 //! @brief Referee Box--do we listen to commands? See StrategyController
 //! @details Chooses between listening to RefComm, and always choosing TestStrategy.
-#define REFBOX_LISTEN_ENABLED 0
+#define REFBOX_LISTEN_ENABLED 1
 
 //!@}
 
